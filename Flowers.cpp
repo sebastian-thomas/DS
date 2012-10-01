b8d07b46518769d7835f2f432cba7125b5881bcb
Sebastian Thomas <mailsebastianthomas@gmail.com>
2012-10-01 19:07:17 +0530

    flowers 1st commit

---
+++ Flowers.cpp
@@ -0,0 +1,53 @@
+
+#include<iostream>
+using namespace std;
+
+void sort_cost(int a[] , int n, int k)
+{
+	int small,pos,temp;
+	
+	  
+   for(int i=0; i<n ; ++i)
+   {
+   	small=a[i];
+   	pos=i;
+   	for(int j=i;j<n;++j)
+   	{
+   	   if(a[j]<small)
+   	     {
+   	        small=a[j];
+   	        pos=j;	
+   	     }	
+   	}
+   	temp=a[i];
+   	a[i]=a[pos];
+   	a[pos]=temp;
+   	
+   }	
+}
+
+int main()
+{
+	 int n,k,totalcost=0,noflowers=0,kcount=0;
+	 int *cost;
+	 cin>>n>>k;
+	 cost= new int[n];
+	 for(int i=0; i<n ;++i)
+	  cin>>cost[i];
+	 
+	 sort_cost(cost, n ,k);
+
+    for(int i=0; i<n; ++i)
+    {
+        	totalcost+= noflowers*cost[i];
+        	kcount++;
+        	if(kcount==k)
+        	{
+        	 	kcount=0;
+        	 	noflowers++;
+        	}
+        	
+    }	 
+	 cout<<totalcost;
+    return 0;	
+}
